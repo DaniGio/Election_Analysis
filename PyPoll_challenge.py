011e49630eadca2f5796de41a05920f40d35eaf8
DaniGio <59997215+DaniGio@users.noreply.github.com>
2020-02-01 18:14:28 +1000

    final update

--- PyPoll_challenge.py
+++ PyPoll_challenge.py
@@ -31,7 +31,7 @@ counties_list = []
 #Declare a new Dictionary
 candidate_votes = {}
 counties_votes = {}
-# Winning Candidate and Winning Count Tracker
+# Winning Candidate, Winning Count Tracker and County
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
@@ -64,10 +64,6 @@ with open(file_to_load) as election_data:
              # Add a vote to that candidate's count. This formula is the same as number = number + 1
         candidate_votes[candidate_name] += 1
 
-          # Print each row in the CSV file.
-    # for row in file_reader:
-        # Add to the total vote count. This formula is the same as number = number + 1
-        # total_votes += 1
         # Print the conties name from each row
         county_name = row[1]
            # If the counties does not match any existing county...
